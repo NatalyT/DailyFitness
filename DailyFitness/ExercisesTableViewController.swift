8ace7a26de93b7225697a6d53633212bcc2366cf
Nataly <ngranetchi@gmail.com>
2018-11-19 18:39:34 +0100

    Collect exercise information

--- DailyFitness/ExercisesTableViewController.swift
+++ DailyFitness/ExercisesTableViewController.swift
@@ -8,6 +8,15 @@
 
 import UIKit
 
+struct ExerciseInfo {
+    var id: Int
+    var category: String
+    var name: String
+    var equipment: String
+    var muscles: String
+    var imageURL: String
+}
+
 class ExercisesTableViewController: UITableViewController {
     
     private let reuseIdentifier = "exerciseCell"
@@ -16,29 +25,64 @@ class ExercisesTableViewController: UITableViewController {
     var categoriesList = [Category]()
     var equipmentList = [Item]()
     var exercisesList = [Exercise]()
+    var exercises = [ExerciseInfo]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        fetchJSONData()
+        getExerciseInfo()
+    }
+    
+    func fetchJSONData() {
         if let m = Muscles.fetchJson() {
             musclesList = m.results ?? []
         }
-//        print(musclesList)
+        //        print(musclesList)
         
         if let c = Categories.fetchJson() {
             categoriesList = c.results ?? []
         }
-//        print(categoriesList)
+        //        print(categoriesList)
         
         if let e = Equipment.fetchJson() {
             equipmentList = e.results ?? []
         }
-//        print(equipmentList)
+        //        print(equipmentList)
         
         if let ex = Exercises.fetchJson() {
             exercisesList = ex.results
         }
-        print(exercisesList)
+//        print(exercisesList)
+    }
+    
+    func getExerciseInfo() {
+        for ex in exercisesList {
+            let category = categoriesList.first (where: { $0.id == ex.category })
+            let newExercise = ExerciseInfo(id: ex.id ?? 0, category: category?.name ?? "", name: ex.name ?? "", equipment: getEquipmentList(equipment: ex.equipment!), muscles: getMusclesList(muscles: ex.muscles!), imageURL: "")
+            exercises.append(newExercise)
+        }
+        print(exercises)
+    }
+    
+    private func getMusclesList(muscles: [Int]) -> String {
+        var muscleNamesString = ""
+        for muscle in muscles {
+            let m = musclesList.first (where: { $0.id == muscle})
+            muscleNamesString.append(contentsOf: (m?.name)!)
+            muscleNamesString.append(contentsOf: ", ")
+        }
+        return String(muscleNamesString.dropLast(2))
+    }
+    
+    private func getEquipmentList(equipment: [Int]) -> String {
+        var equipmentNamesString = ""
+        for equipmentItem in equipment {
+            let e = equipmentList.first (where: { $0.id == equipmentItem })
+            equipmentNamesString.append(contentsOf: (e?.name)!)
+            equipmentNamesString.append(contentsOf: ", ")
+        }
+        return String(equipmentNamesString.dropLast(2))
     }
 
     // MARK: - Table view data source
